144937dcc4c0decbd492e61e58e8eceac7ba51c7
MarcusFredericks2021 <marcusfredericks2021@gmail.com>
2024-03-18 12:36:16 -0400

    Added my-app to project

---
+++ my-app/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div>test</div>;
+}
+// use browser router eventually when there are more pages
+//for now return jsx page as a component
+export default App;
---
+++ my-app/src/pages/landing.jsx
@@ -0,0 +1,8 @@
+/*
+landing/about page
+Title, and content
+explain what this is, why the stats are important, and why the future telemetry function is important
+explain why you are choosing to do this project
+add pictures
+simple styling and structure
+*/
